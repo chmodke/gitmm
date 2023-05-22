3542735a50d46c7f0045989d048438ba6bf1c372
chmodke <13772804655@163.com>
2023-05-22 22:35:14 +0800

    refactor: 日志重构

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
--- log/log.go
+++ log/log.go
@@ -2,16 +2,22 @@ package log
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-homedir"
 	"log"
 	"os"
+	"time"
 )
 
 var logger *log.Logger
 var logFile *os.File
+var home, _ = homedir.Dir()
+var logPath = fmt.Sprintf("%s/.gitmm", home)
+var logfile = fmt.Sprintf("%s/gitmm-%s.log", logPath, time.Now().Format("20060102"))
 
 func init() {
 	var err error
-	logFile, err = os.OpenFile("gitmm.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	os.MkdirAll(logPath, 0750)
+	logFile, err = os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0640)
 	if err != nil {
 		fmt.Println("open file error :", err)
 		return
